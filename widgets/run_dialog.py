20f1ecf9ade61d6c58f0a50903e0cfe3b246af55
StanleyWangTW <x039516@gmail.com>
2024-03-22 01:09:20 +0800

    pbar track file

--- widgets/run_dialog.py
+++ widgets/run_dialog.py
@@ -1,34 +1,61 @@
+import os
+
 from PySide6 import QtWidgets
 import PySide6.QtCore as Qt
 
 
 class RunDialog(QtWidgets.QDialog):
 
-    def __init__(self, fnum=None):
+    def __init__(self, fnum, output_dir):
         super(RunDialog, self).__init__()
-        self.value = 0
-        self.setup_ui(fnum)
+        self.output_dir = output_dir
+        self.fnum = fnum
+        self.setup_ui()
 
         self.timer = Qt.QTimer(self)
-        self.timer.timeout.connect(self.update_progress)
+        self.timer.timeout.connect(self.check_progress)
         self.timer.start(100)
 
-    def setup_ui(self, fnum):
-        self.setWindowTitle('Running')
+    def setup_ui(self):
+        self.setWindowTitle(f'Running 1 of {self.fnum}')
         v_layout = QtWidgets.QVBoxLayout()
 
         self.pbar = QtWidgets.QProgressBar()
-        self.pbar.setValue(1)
+        self.pbar.setValue(0)
+        self.pbar.setMaximum(self.fnum)
         v_layout.addWidget(self.pbar)
         self.setLayout(v_layout)
 
-        # self.pbar.valueChanged.connect(self.finish)
+        self.pbar.valueChanged.connect(self.if_finish)
 
-    def update_progress(self):
-        self.value += 1
-        self.pbar.setValue(self.value + 1)
+    def check_progress(self):
+        num_of_files = len(os.listdir(self.output_dir))
+        if num_of_files != self.pbar.value():
+            self.update_progress(num_of_files)
 
-    # def finish(self, value):
-    #     self.done()
-    # if value == self.pbar.maximum():
-    # self.accept()
+    def update_progress(self, num_of_files):
+        self.pbar.setValue(len(os.listdir(self.output_dir)))
+        self.setWindowTitle(f'Running {self.pbar.value() + 1} of {self.fnum}')
+
+    def if_finish(self, value):
+        if value == self.pbar.maximum():
+            finish_dialog = FinishDialog()
+            finish_dialog.exec()
+            self.accept()
+
+
+class FinishDialog(QtWidgets.QDialog):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setWindowTitle("FINISH")
+        QBtn = QtWidgets.QDialogButtonBox.Ok
+        self.buttonBox = QtWidgets.QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+
+        self.layout = QtWidgets.QVBoxLayout()
+        message = QtWidgets.QLabel('Segmentation finished')
+        self.layout.addWidget(message)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
