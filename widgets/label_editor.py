cf19d58fe6181ec6d6ad76809e136290d816fe53
StanleyWangTW <x039516@gmail.com>
2024-05-12 22:58:47 +0800

    new label creater

--- widgets/label_editor.py
+++ widgets/label_editor.py
@@ -1,5 +1,5 @@
-from PySide6.QtWidgets import QWidget, QLabel, QHBoxLayout, QVBoxLayout, QFrame, QListWidget, QListWidgetItem, QSizePolicy, QTabWidget
-from PySide6.QtGui import QImage, QPixmap
+from PySide6.QtWidgets import *
+from PySide6.QtGui import QImage, QPixmap, Qt, QIntValidator
 import numpy as np
 
 class QVLine(QFrame):
@@ -48,24 +48,153 @@ class Label(QWidget):
 class  LabelList(QListWidget):
     def __init__(self):
         super(LabelList, self).__init__()
-        self.setLayout(QVBoxLayout())
 
-    # def addLabel(self, number, name, rgba):
-    #     row = Label(number, name, rgba)
+        available_labels = QGroupBox('Available Labels')
+
+        vbox = QVBoxLayout()
+        hbox = QHBoxLayout()
+        new_btn = QPushButton('new')
+        hbox.addWidget(new_btn)
+        hbox.addWidget(QPushButton('delete'))
+        vbox.addLayout(hbox)
+        self.setLayout(vbox)
+
+        new_btn.clicked.connect(self.create_label)
+
+
+    def addLabel(self, number, name, rgba):
+        row = Label(number, name, rgba)
+        item = QListWidgetItem(self)
+        item.setSizeHint(row.minimumSizeHint())
+        self.addItem(item)
+        self.setItemWidget(item, row)
+
+    # def addLabel(self, number, label):
+    #     row = Label(number, label.name, label.rgba)
     #     item = QListWidgetItem(self)
     #     item.setSizeHint(row.minimumSizeHint())
     #     self.addItem(item)
     #     self.setItemWidget(item, row)
 
-    def addLabel(self, number, label):
-        row = Label(number, label.name, label.rgba)
-        item = QListWidgetItem(self)
-        item.setSizeHint(row.minimumSizeHint())
-        self.addItem(item)
-        self.setItemWidget(item, row)
+    def create_label(self):
+        label_picker = LabelCreater()
+        number, name, rgba = label_picker.create_label()
+        if number is not None:
+            self.addLabel(number, name, rgba)
 
 
 class LeftWidget(QTabWidget):
     def __init__(self):
         super(LeftWidget, self).__init__()
         self.setTabShape(self.TabShape.Rounded)
+
+
+class LabelCreater(QDialog):
+    def __init__(self):
+        super(LabelCreater, self).__init__()
+        self.setModal(True)
+
+        self.color = [255, 0, 0, 255]
+
+        self.initUI()
+        self.connect_signals()
+
+    def initUI(self):
+        self.setWindowTitle('Label Picker - TigerBx')
+
+        vbox = QVBoxLayout()
+
+        self.number = QLineEdit()
+        self.number.setValidator(QIntValidator())
+        vbox.addWidget(QLabel('Label Value:'))
+        vbox.addWidget(self.number)
+
+        self.name = QLineEdit()
+        vbox.addWidget(QLabel('Name:'))
+        vbox.addWidget(self.name)
+
+        color_hbox = QHBoxLayout()
+        color_hbox.addWidget(QLabel('Color:'))
+        self.color_btn = QPushButton()
+        self.color_btn.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        self.set_color_btn_color()
+        color_hbox.addWidget(self.color_btn)
+        vbox.addLayout(color_hbox)
+
+        opacity_vbox = QVBoxLayout()
+        opacity_vbox.addWidget(QLabel('Opacity:'))
+        hbox = QHBoxLayout()
+        self.opacity_ledt = QLineEdit('255')
+        self.opacity_ledt.setValidator(QIntValidator())
+        self.opacity_ledt.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        hbox.addWidget(self.opacity_ledt)
+        self.opacity_slider = QSlider(Qt.Horizontal)
+        self.opacity_slider.setRange(0, 255)
+        hbox.addWidget(self.opacity_slider)
+        opacity_vbox.addLayout(hbox)
+        vbox.addLayout(opacity_vbox)
+
+        btn_hbox = QHBoxLayout()
+        self.ok_btn = QPushButton('OK')
+        self.cancel_btn = QPushButton('CANCEL')
+        btn_hbox.addWidget(self.ok_btn)
+        btn_hbox.addWidget(self.cancel_btn)
+        vbox.addLayout(btn_hbox)
+        self.setLayout(vbox)
+
+    def connect_signals(self):
+        self.color_btn.clicked.connect(self.pick_color)
+        self.opacity_slider.valueChanged.connect(self.set_opacity)
+        self.ok_btn.clicked.connect(self.accept)
+        self.cancel_btn.clicked.connect(self.reject)
+
+    def create_label(self):
+        if self.exec() == QDialog.Accepted:
+            try:
+                number = int(self.number.text())
+            except ValueError:
+                self.show_error_message('Label value should be an integer.')
+                return None, None, None
+
+            self.color[-1] = int(self.opacity_ledt.text())
+            name = self.name.text()
+            print(self.color)
+            return number, name, self.color
+        else:
+            return None, None, None
+
+    def pick_color(self):
+        color_dlg = QColorDialog()
+        new_color = color_dlg.getColor()
+        if new_color.isValid():
+            self.color = list(new_color.getRgb())
+            self.opacity_ledt.setText(str(new_color.alpha()))
+            self.set_color_btn_color()
+
+    def set_opacity(self, value):
+        self.opacity_ledt.setText(str(value))
+        self.color[-1] = value
+
+    def set_color_btn_color(self):
+        square = np.zeros((10, 10, 3))
+        square[..., :3] = self.color[:3]
+        pixmap = QPixmap.fromImage(QImage(square.astype(np.uint8),
+                                          square.shape[0],
+                                          square.shape[1],
+                                          square.shape[1] * 3,
+                                          QImage.Format_RGB888))
+        self.color_btn.setIcon(pixmap)
+
+    def show_error_message(self, message):
+        msg_box = QMessageBox(QMessageBox.Critical, 'Input Error', message)
+        msg_box.exec()
+
+
+if __name__ == '__main__':
+    import sys
+    from PySide6.QtWidgets import QApplication
+
+    app = QApplication(sys.argv)
+    win  = LabelList()
+    win.show()
+    app.exec()
